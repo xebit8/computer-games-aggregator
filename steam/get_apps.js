1cc6e6f1d1716f244c7069ab9c5e6b4327e9bbde
Anton <52106716+Xebit8@users.noreply.github.com>
2024-12-01 00:44:59 +0300

    got the problem with sanctions and authorization requirement

---
+++ steam/get_apps.js
@@ -0,0 +1,45 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const getAppsList = require("./get_apps_list.js");
+
+
+(async function getApps() {
+    try 
+    {
+        const appsList = await getAppsList();
+        //console.log(appsList);
+        console.log("--------------------------------------------------------");
+        for (let app of appsList)
+        {
+            console.log(app);
+            await getAppData(app);
+            console.log("--------------------------------------------------------");
+        }
+    } catch (error) {
+        console.error("Couldn't get games info.", error);
+    }
+})();
+
+async function getAppData(app) {
+    try {
+        const delay = seconds => new Promise(resolve => setTimeout(resolve, seconds * 1000));
+        await delay(2);
+        const baseUrl = "https://store.steampowered.com/app/";
+        console.log("Ссылка на игру:", baseUrl + app.appid);
+        const response = await axios.get(baseUrl + app.appid);
+        const $ = cheerio.load(response.data);
+        const $error = $(".error").text();
+        console.log("Ошибка (если есть):", $error || "Нет");
+        const $releaseDate = $(".date").text();
+        console.log("Дата релиза:", $releaseDate || "Не найдено");
+        // const $gamePurchaseArea = $(".game_area_purchase_game ");
+        // if ($totalPrice === "") {
+        //     $totalPrice = "Yet to be announced..."
+        // }
+        // else if ($totalPrice === "")
+        // console.log("found: " + $totalPrice.text().trim());
+        
+    } catch (error) {
+        console.error("Couldn't get info from Steam's game page", error);
+    }
+}
--- steam/get_apps_list.js
+++ steam/get_apps_list.js
@@ -1,16 +1,19 @@
-import { get } from "axios";
+const axios = require("axios");
 
 
-export default async function getAppsList() {
+module.exports = async function getAppsList() {
     try {
-        const response = await get('https://api.steampowered.com/ISteamApps/GetAppList/v2/');
+        const response = await axios.get('https://api.steampowered.com/ISteamApps/GetAppList/v2/');
+        // const apps = response.data.applist.apps.slice(-3);
         const apps = response.data.applist.apps;
-        console.log(apps);
-        for (let i = 0; i < 100; i++)
+        //console.log(apps);
+        let someApps = []
+        for (let i = 0; i < apps.length; i++)
         {
-            console.log(apps[i]);
+            if (apps[i].name === "Dead by Daylight" | apps[i].name === "A Shlong Adventure") someApps.push(apps[i])
+            //console.log(apps[i]);
         }
-        return apps;
+        return someApps.concat(apps.slice(-3));
     } catch (error) {
         console.error("Couldn't get info from Steam Web API.", error);
     }
