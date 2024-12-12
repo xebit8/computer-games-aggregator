06f49a592d69eeef5d5b1a13cba9eaaf1b0f6869
Anton <52106716+Xebit8@users.noreply.github.com>
2024-12-12 21:57:27 +0300

    add news and top games scrapers

---
+++ epic games/parse_news.js
@@ -0,0 +1,31 @@
+const axios = require("axios");
+
+
+const query = "";
+
+(async function parseNews() {
+    try {
+        const news = [];
+        let start = 0;
+        let total = 100;
+        let count = 500;
+        do {
+            const response = await axios.get(`https://store-content-ipv4.ak.epicgames.com/api/ru/content/blog-posts?country=RU&locale=ru&count=${count}&start=${start}`);
+            const data = response.data;
+            data.elements.forEach(element => {
+                const url = "https://store.epicgames.com"+element.url.replace("/blog", "/news");
+                news.push({
+                    "title": element.title,
+                    "short_description": element.short,
+                    "url": url,
+                });
+            });
+            total = data.paging.total;
+            start += data.paging.count;
+        } while (start < total)
+            // console.log(news);
+            return news;
+    } catch (error) {
+        console.error("Error!", error);
+    }
+})();
---
+++ epic games/parse_top_games.js
@@ -0,0 +1,46 @@
+const axios = require("axios");
+
+
+const query = `
+query collectionLayoutQuery(
+  $locale: String = "ru-RU", 
+  $country: String!, 
+  $slug: String
+) {
+  Storefront {
+    collectionLayout(locale: $locale, slug: $slug) {
+      collectionOffers {
+        title
+        price(country: $country) {
+          totalPrice {
+            originalPrice
+          }
+        }
+      }
+    }
+  }
+}`;
+
+const variables = {
+  country: "RU", 
+  locale: "ru-RU",
+  slug: "most-popular",
+};
+
+module.exports = async function parseTopGames() {
+    try {
+      const topGames = [];
+      const response = await axios.post('https://graphql.epicgames.com/graphql', { query, variables });
+      const data = response.data.data.Storefront.collectionLayout.collectionOffers;
+      data.forEach((element, index) => {
+        const title = element.title;
+        topGames.push({
+          "position": index+1, 
+          "title": title,
+        });
+      });
+      return topGames;
+    } catch (error) {
+      console.error("Error!", error);
+    }
+};
